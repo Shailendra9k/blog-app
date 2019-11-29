abbd91ccad7986ed6297dacd3daf2f4a7557ba4f
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-11-29 10:13:06 +0200

    man components added

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header className="masthead myhead">
+        <div className="overlay"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <div className="site-heading">
+                <h1>Quote Blog</h1>
+                <span className="subheading">Collection of Some Quotes</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
